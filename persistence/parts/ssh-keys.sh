77a7a47eb55103e7e1f29fe0e60b4a998b6e9cfa
McSquizzy <mettler.lukas@bluewin.ch>
2022-04-20 23:47:29 +0200

    persistence: update modify ssh keys

--- persistence/parts/ssh-keys.sh
+++ persistence/parts/ssh-keys.sh
@@ -2,14 +2,6 @@
 # SSH Keys #
 ############
 
-# check if $HOME variable is set
-USER=$(whoami 2>/dev/null || echo "User is unknown")
-if [ ! "$HOME" ]; then
-  if [ -d "/home/$USER" ];
-    then HOME="/home/$USER";
-  fi
-fi
-
 if [ "$SSH" ]; then
   echo "
     __  __           _ _  __         ____ ____  _   _   _  __                      
@@ -21,20 +13,31 @@ if [ "$SSH" ]; then
   "
   sleep 1
 
+  # check if local ssh server is running
+  fuTITLE "Check if local ssh server is running ..."
+  sleep 2
+  if ps aux | grep sshd | grep -v grep 2>/dev/null; then
+    fuMESSAGE "Local ssh server is running"
+  elif netstat -plant | grep :22 | grep LISTEN 2>/dev/null; then
+    fuMESSAGE "Local ssh server is listening on port 22"
+  else
+    fuERROR "Probably no ssh server running on this host"
+  fi
+
   # check if $HOME variable is set
-  USER=$(whoami 2>/dev/null || echo "User is unknown")
+  command -v whoami 2>/dev/null) || fuERROR "command \"whoami\" not found"
+  $USER=$(whoami 2>/dev/null)
   if [ ! "$HOME" ]; then
-    if [ -d "/home/$USER" ];
-        then HOME="/home/$USER";
+    if [ -d "/home/$USER" ]; then
+      HOME="/home/$USER"
     fi
   fi
 
-  current_user=$(whoami 2>/dev/null)
-
+  fuTITLE "Trying to add given SSH public key to authorized_keys file of user $USER ..."
+  sleep 2
   if [ "$PUBKEY" != "" ]; then
-    fuTITLE "Trying to add given SSH public key to authorized_keys file of user $current_user ..."
     if [ -d "$HOME/.ssh" ]; then
-      if echo "$PUBKEY" >> "$HOME"/.ssh/authorized_keys; then TRYCHMOD=""; else fuERROR "unable to write authorized_keys" && TRYCHMOD="1"; fi
+      if echo "$PUBKEY" >> "$HOME"/.ssh/authorized_keys; then TRYCHMOD=""; else SSHOK="" && fuERROR "unable to write authorized_keys" && TRYCHMOD="1"; fi
       if [ "$TRYCHMOD" ]; then
         CHMOD=$(command -v chmod 2>/dev/null) || fuERROR "command \"chmod\" not found"
         $CHMOD 700 "$HOME"/.ssh 2>/dev/null
