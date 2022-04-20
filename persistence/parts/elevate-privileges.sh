28798450c72c6684d122f4aa59560a1fbaac7cd8
McSquizzy <mettler.lukas@bluewin.ch>
2022-04-20 23:46:27 +0200

    persistence: adjust ideas

--- persistence/parts/elevate-privileges.sh
+++ persistence/parts/elevate-privileges.sh
@@ -1,6 +1,11 @@
 
 # with root
 # add user to sudo group
-usermod -a -G sudo <usernam>
+usermod -a -G sudo <username>
+
+
+fuTITLE "trying to add user $USER to sudo group"
+
+usermod -a -G sudo <username> 2>/dev/null
 
 
