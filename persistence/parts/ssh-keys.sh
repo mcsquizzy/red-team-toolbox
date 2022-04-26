95d0c7831fd7952ad39289ed501727d8ce07fc52
McSquizzy <mettler.lukas@bluewin.ch>
2022-04-26 17:25:59 +0200

    persistence: update

--- persistence/parts/ssh-keys.sh
+++ persistence/parts/ssh-keys.sh
@@ -24,35 +24,42 @@ if [ "$SSH" ]; then
     fuERROR "Probably no ssh server running on this host"
   fi
 
-  # check if $HOME variable is set
-  command -v whoami 2>/dev/null) || fuERROR "command \"whoami\" not found"
-  $USER=$(whoami 2>/dev/null)
-  if [ ! "$HOME" ]; then
-    if [ -d "/home/$USER" ]; then
-      HOME="/home/$USER"
-    fi
-  fi
-
-  fuTITLE "Trying to add given SSH public key to authorized_keys file of user $USER ..."
-  sleep 2
-  if [ "$PUBKEY" != "" ]; then
-    if [ -d "$HOME/.ssh" ]; then
-      if echo "$PUBKEY" >> "$HOME"/.ssh/authorized_keys; then TRYCHMOD=""; else SSHOK="" && fuERROR "unable to write authorized_keys" && TRYCHMOD="1"; fi
-      if [ "$TRYCHMOD" ]; then
-        CHMOD=$(command -v chmod 2>/dev/null) || fuERROR "command \"chmod\" not found"
-        $CHMOD 700 "$HOME"/.ssh 2>/dev/null
-        echo "$PUBKEY" >> "$HOME"/.ssh/authorized_keys && SSHOK="1" && fuMESSAGE "authorized_keys updated"
-      else
-        SSHOK="1" && echo "authorized_keys updated"
+  # set current user to $USER
+  if WHOAMI=$(command -v whoami 2>/dev/null); then
+    USER=$($WHOAMI 2>/dev/null)
+    # check if $HOME variable is set
+    if [ ! "$HOME" ]; then
+      if [ -d "/home/$USER" ]; then
+        HOME="/home/$USER"
       fi
-    else
-      fuINFO "No .ssh directory exists, creating one ..."
-      MKDIR=$(command -v mkdir 2>/dev/null) || fuERROR "command \"mkdir\" not found"
-      CHMOD=$(command -v chmod 2>/dev/null) || fuERROR "command \"chmod\" not found"
-      $MKDIR "$HOME"/.ssh 2>/dev/null && $CHMOD 700 "$HOME"/.ssh 2>/dev/null
-      if echo "$PUBKEY" >> "$HOME"/.ssh/authorized_keys; then SSHOK="1" && fuMESSAGE "authorized_keys updated"; else SSHOK="" && fuERROR "unable to write authorized_keys"; fi
     fi
   else
-    fuERROR "No public key given."
+    fuERROR "command \"whoami\" not found"
+  fi
+
+  # get local ip addresses
+  LOCAL_IP=$(ip a | grep -vi docker | grep -Eo 'inet[^6]\S+[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | awk '{print $2}' | grep -E "^10\.|^172\.|^192\.168\.|^169\.254\.")
+
+  fuTITLE "Trying to add given ssh public key to authorized_keys file of user \"$USER\" ..."
+  sleep 2
+
+  if [ -d "$HOME/.ssh" ]; then
+    if echo "$PUBKEY" >> "$HOME"/.ssh/authorized_keys; then TRYCHMOD=""; else SSHOK="" && fuERROR "unable to write authorized_keys" && TRYCHMOD="1"; fi
+    if [ "$TRYCHMOD" ]; then
+      if CHMOD=$(command -v chmod 2>/dev/null); then
+        $CHMOD 700 "$HOME"/.ssh 2>/dev/null
+        echo "$PUBKEY" >> "$HOME"/.ssh/authorized_keys && SSHOK="1" && fuMESSAGE "authorized_keys updated"
+      else 
+        fuERROR "command \"chmod\" not found"
+      fi
+    else
+      SSHOK="1" && echo "authorized_keys updated"
+    fi
+  else
+    fuINFO "No .ssh directory exists, creating one ..."
+    MKDIR=$(command -v mkdir 2>/dev/null) || fuERROR "command \"mkdir\" not found"
+    CHMOD=$(command -v chmod 2>/dev/null) || fuERROR "command \"chmod\" not found"
+    $MKDIR "$HOME"/.ssh 2>/dev/null && $CHMOD 700 "$HOME"/.ssh 2>/dev/null
+    if echo "$PUBKEY" >> "$HOME"/.ssh/authorized_keys; then SSHOK="1" && fuMESSAGE "authorized_keys updated"; else SSHOK="" && fuERROR "unable to write authorized_keys"; fi
   fi
 fi
