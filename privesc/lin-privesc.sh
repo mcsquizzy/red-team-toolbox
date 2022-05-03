42b213de4e3ffcdeb6dcab56065ec87aeef219b7
McSquizzy <mettler.lukas@bluewin.ch>
2022-05-03 16:00:26 +0200

    privesc: update parameter handling

--- privesc/lin-privesc.sh
+++ privesc/lin-privesc.sh
@@ -74,38 +74,24 @@ fuMESSAGE() {
   echo "$BBLUE---$NC $1 $NC"
 }
 
-# Check for root permissions
-fuGOT_ROOT() {
-#fuINFO "Checking for root"
-if ([ -f /usr/bin/id ] && [ "$(/usr/bin/id -u)" -eq "0" ]) || [ "`whoami 2>/dev/null`" = "root" ]; then
-  IAMROOT="1"
-  fuINFO "You are already root!"
-  sleep 2
-  echo
-else
-  IAMROOT=""
-  #fuMESSAGE "You are not root"
-  echo
-fi
-}
 
 #####################################
 # Check the command line parameters #
 #####################################
 
-while getopts "hs:" opt; do
-  case "$opt" in
-    h|\?)
-      echo
-      echo "Usage: sh $0 [-h] [-xxxx]"
-      echo
-      echo "-xxx <xxxxx>"
-      echo "  Trying to xxxx"
-      echo
-      exit;;
-    x) blablabla;;
-    esac
-done
+#while getopts "hs:" opt; do
+#  case "$opt" in
+#    h|\?)
+#      echo
+#      echo "Usage: sh $0 [-h] [-xxxx]"
+#      echo
+#      echo "-xxx <xxxxx>"
+#      echo "  Trying to xxxx"
+#      echo
+#    x) blablabla;;
+##      exit;;
+#    esac
+#done
 
 
 ##########
@@ -116,13 +102,6 @@ fuBANNER
 echo
 echo "${BYELLOW}Advisory: ${BBLUE}Use this script for educational purposes and/or for authorized penetration testing only. The author is not responsible for any misuse or damage caused by this script. Use at your own risk.$NC"
 echo
-sleep 1
-
-#####################
-# Checking for root #
-#####################
-
-fuGOT_ROOT
 sleep 1
 
 
@@ -140,7 +119,6 @@ echo "
  |_____|_|_| |_|_|   |_____/_/   \_\____/ 
                                           
 "
-sleep 1
 
 VERSION="ng"
 ADVISORY="This script should be used for authorized penetration testing and/or educational purposes only. Any misuse of this software will not be the responsibility of the author or of any other collaborator. Use it at your own computers and/or with the computer owner's permission."
