03e4ab1697240e055aba803615032d3815d194ce
McSquizzy <mettler.lukas@bluewin.ch>
2022-05-05 16:32:13 +0200

    internal-recon: add more checks

--- internal-recon/internal-recon.sh
+++ internal-recon/internal-recon.sh
@@ -25,6 +25,7 @@ hostname=`hostname 2>/dev/null`
 
 mySYSTEMFILE="${hostname}_system_info.txt"
 myNETWFILE="${hostname}_network_info.txt"
+myUSERFILE="${hostname}_user_info.txt"
 
 
 #############
@@ -217,6 +218,22 @@ if [ "$architecture" ]; then
 else
   echo "  $BYELLOW Architecture:$NC unknown"
 fi
+
+# check if selinux is enabled
+fuCHECKS "Check if SELinux is enabled"
+sestatus=$(sestatus 2>/dev/null)
+if [ "$sestatus" ]; then fuOK && echo "$sestatus"; else fuNOTOK; fi
+
+# list available shells
+fuCHECKS "All available shells"
+shells=$(cat /etc/shells 2>/dev/null)
+if [ "$shells" ]; then fuOK && echo "$shells"; else fuNOTOK; fi
+
+# password policy
+fuCHECKS "Password policy and password encryption method"
+pwpolicy=$(grep "^PASS_MAX_DAYS\|^PASS_MIN_DAYS\|^PASS_WARN_AGE\|^LOGIN_RETRIES\|^ENCRYPT_METHOD" /etc/login.defs 2>/dev/null)
+if [ "$pwpolicy" ]; then fuOK && echo "$pwpolicy"; else fuNOTOK; fi
+
 }
 
 #######################
@@ -228,29 +245,28 @@ network_info() {
 fuTITLE "Network information"
 sleep 1
 
+# current IP(s)
 fuCHECKS "Current IP"
 currentip=$(ip a | grep -vi docker | grep -Eo 'inet[^6]\S+[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | awk '{print $2}' | grep -E "^10\.|^172\.|^192\.168\.|^169\.254\." 2>/dev/null)
 currentif=$(ifconfig | grep -vi docker | grep -Eo 'inet[^6]\S+[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | awk '{print $2}' | grep -E "^10\.|^172\.|^192\.168\.|^169\.254\." 2>/dev/null)
-if [ "$currentip" ]; then
-  fuOK && echo "$currentip"
-elif [ "$currentif" ]; then
-  fuOK && echo "$currentif"
-else
-  fuNOTOK
-fi
+if [ "$currentip" ]; then fuOK && echo "$currentip"; elif [ "$currentif" ]; then fuOK && echo "$currentif"; else fuNOTOK; fi
 
+# dns info
 fuCHECKS "DNS info"
 dnsinfo=$(grep "nameserver" /etc/resolv.conf 2>/dev/null || systemd-resolve --status 2>/dev/null)
 if [ "$dnsinfo" ]; then fuOK && echo "$dnsinfo"; else fuNOTOK; fi
 
+# route info
 fuCHECKS "Route info"
 defroute=$(route || ip r | grep default) 2>/dev/null
 if [ "$defroute" ]; then fuOK && echo "$defroute"; else fuNOTOK; fi
 
+# open TCP connections
 fuCHECKS "Listening TCP connections"
 tcplisten=$(netstat -tlpn || ss -tln) 2>/dev/null
 if [ "$tcplisten" ]; then fuOK && echo "$tcplisten"; else fuNOTOK; fi
 
+# open UDP connections
 fuCHECKS "Listening UDP connections"
 udplisten=$(netstat -ulpn || ss -uln) 2>/dev/null
 if [ "$udplisten" ]; then fuOK && echo "$udplisten"; else fuNOTOK; fi
@@ -266,73 +282,99 @@ user_info() {
 fuTITLE "User information"
 sleep 1
 
+# current user info
 fuCHECKS "Current user info"
 currentuser=$(id 2>/dev/null)
 if [ "$currentuser" ]; then fuOK && echo "$currentuser"; else fuNOTOK; fi
 
+# check path variable of current user
+fuCHECKS "Path config of current user"
+pathconfig=$(echo $PATH 2>/dev/null)
+if [ "$pathconfig" ]; then fuOK && echo "$pathconfig"; else fuNOTOK; fi
+
+# other users that have logged onto the system
 fuCHECKS "Users that have also logged onto the system"
 lastloggedonusers=$(lastlog 2>/dev/null | grep -v Never)
 if [ "$lastloggedonusers" ]; then fuOK && echo "$lastloggedonusers"; else fuNOTOK; fi
 
+# other users that are logged on right now
 fuCHECKS "Users that are logged on right now"
 loggedonusers=$(w -h || who || users) 2>/dev/null
 if [ "$loggedonusers" ]; then fuOK && echo "$loggedonusers"; else fuNOTOK; fi
 
+# users with a login shell
 fuCHECKS "All users with a login shell"
 shellusers=$(cat /etc/passwd 2>/dev/null | grep -i "sh$" | cut -d ":" -f 1)
 if [ "$shellusers" ]; then fuOK && echo "$shellusers"; else fuNOTOK; fi
 
+# all users
 fuCHECKS "All users and the group they belong to"
 allusers=$(cut -d":" -f1 /etc/passwd 2>/dev/null | while read i; do id $i; done 2>/dev/null | sort)
 if [ "$allusers" ]; then fuOK && echo "$allusers"; else fuNOTOK; fi
 
+# all users within a admin group
 fuCHECKS "All users within a admin group (admin, root, sudo, wheel)"
 allusers=$(cut -d":" -f1 /etc/passwd 2>/dev/null | while read i; do id $i; done 2>/dev/null | sort | grep -E "^adm|admin|root|sudo|wheel")
 if [ "$allusers" ]; then fuOK && echo "$allusers"; else fuNOTOK; fi
 
+# user loggon history
 fuCHECKS "Last logons"
 lastlogons=$(last -Faiw | head -30 2>/dev/null || last | head -30 2>/dev/null)
 if [ "$lastlogons" ]; then fuOK && echo "$lastlogons"; else fuNOTOK; fi
 
+# is /etc/shadow readable
 fuCHECKS "Can we read /etc/shadow file?"
 readshadow=$(cat /etc/shadow 2>/dev/null)
 if [ "$readshadow" ]; then fuOK && echo "$readshadow"; else fuNOTOK; fi
 
+# hashes in /etc/passwd
 fuCHECKS "Are there hashes in the /etc/passwd file?"
 hashesinpasswd=$(grep -v '^[^:]*:[x]' /etc/passwd 2>/dev/null)
 if [ "$hashesinpasswd" ]; then fuOK && echo "$hashesinpasswd"; else fuNOTOK; fi
 
+# is /etc/master.passwd readable
 fuCHECKS "Can we read /etc/master.passwd file?"
 readmaster=$(cat /etc/master.passwd 2>/dev/null)
 if [ "$readmaster" ]; then fuOK && echo "$readmaster"; else fuNOTOK; fi
 
+# all root accounts (uid = 0)
 fuCHECKS "All root / superuser accounts"
 superuser=$(grep -v -E "^#" /etc/passwd 2>/dev/null | awk -F: '$3 == 0 { print $1}' 2>/dev/null)
 if [ "$superuser" ]; then fuOK && echo "$superuser"; else fuNOTOK; fi
 
-fuCHECKS "Check sudoers configuration"
-sudoers=$(grep -v -e '^$' /etc/sudoers | grep -v "#") 2>/dev/null
-if [ "$sudoers" ]; then fuOK && echo "$sudoers"; else fuNOTOK; fi
-
-fuCHECKS "Check if we can sudo without a password"
-sudopasswd=$(echo "" | sudo -S -l -k) 2>/dev/null
-if [ "$sudopasswd" ]; then fuOK && echo "$sudopasswd"; else fuNOTOK; fi
-
-fuCHECKS "Check who have used sudo in the past"
-whosudo=$(find /home -name .sudo_as_admin_successful 2>/dev/null)
-if [ "$whosudo" ]; then fuOK && echo "$whosudo"; else fuNOTOK; fi
-
+# home directory
 fuCHECKS "Check home directory permissions"
 homedirperms=$(ls -lh /home/ 2>/dev/null)
-if [ "$whosudo" ]; then fuOK && echo "$whosudo"; else fuNOTOK; fi
+if [ "$homedirperms" ]; then fuOK && echo "$homedirperms"; else fuNOTOK; fi
 
+# writable files
 fuCHECKS "Writable files but not owned by me"
 writablefiles=$(find / -writable ! -user $(whoami) -type f ! -path "/proc/*" ! -path "/sys/*" -exec ls -al {} \; 2>/dev/null)
 if [ "$writablefiles" ]; then fuOK && echo "$writablefiles"; else fuNOTOK; fi
 
+# is root allowed to login via SSH
 fuCHECKS "Check if root is permitted to login via ssh"
 rootssh=$(grep "PermitRootLogin " /etc/ssh/sshd_config 2>/dev/null | grep -v "#" | awk '{print  $2}')
-if [ "$rootssh" == "yes" ]; then fuOK && grep "PermitRootLogin " /etc/ssh/sshd_config 2>/dev/null | grep -v "#"; else fuNOTOK; fi
+if [ "$rootssh" = "yes" ]; then fuOK && (grep "PermitRootLogin " /etc/ssh/sshd_config 2>/dev/null | grep -v "#"); else fuNOTOK; fi
+
+####################
+# Sudo Information #
+####################
+
+# sudoers config
+fuCHECKS "Check sudoers configuration"
+sudoers=$(grep -v -e '^$' /etc/sudoers | grep -v "#") 2>/dev/null
+if [ "$sudoers" ]; then fuOK && echo "$sudoers"; else fuNOTOK; fi
+
+# sudo without password
+fuCHECKS "Check if we can sudo without a password"
+sudopasswd=$(echo "" | sudo -S -l -k) 2>/dev/null
+if [ "$sudopasswd" ]; then fuOK && echo "$sudopasswd"; else fuNOTOK; fi
+
+# past sudo uses
+fuCHECKS "Check who have used sudo in the past"
+whosudo=$(find /home -name .sudo_as_admin_successful 2>/dev/null)
+if [ "$whosudo" ]; then fuOK && echo "$whosudo"; else fuNOTOK; fi
 
 }
 
