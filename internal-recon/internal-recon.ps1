bec89d17331f67256204da1cdc8b0dac7a78117d
McSquizzy <mettler.lukas@bluewin.ch>
2022-06-03 22:25:27 +0200

    internal-recon: update, cleanup powershell script

--- internal-recon/internal-recon.ps1
+++ internal-recon/internal-recon.ps1
@@ -4,7 +4,7 @@
 # Global Variables #
 ####################
 
-
+#$hostname = hostname
 #$myUSERFILE = "${hostname}_user_info.txt"
 
 
@@ -17,19 +17,20 @@ function system_info {
 Write-Output "" #newline
 Write-Output "System information"
 Write-Output ""
-Write-Output "User: " $(whoami)
-Write-Output "Home Path: " $(echo $HOME)
-Write-Output "Hostname: " $(hostname)
 
-#Write-Output "Windows Version:"
-#$winversion = Get-ComputerInfo | select WindowsProductName, WindowsVersion
-#Get-ComputerInfo | select WindowsProductName, WindowsVersion, OsHardwareAbstractionLayer
+Write-Output "User: $(whoami)"
+Write-Output "Home Path: $(echo $HOME)"
+Write-Output "Hostname: $(hostname)"
+Write-Output ""
 
-#Start-Sleep -Seconds 6
+# Windows version
+# Todo
 
 # all variables
+Write-Output ""
 Write-Output "All variables:"
 Get-Variable
+Write-Output ""
 
 # Processor Information, System Type
 Write-Output ""
@@ -37,17 +38,15 @@ Write-Output "Processor information, System Type:"
 Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -Property SystemType
 
 # Listing BIOS settings
+Write-Output ""
 Write-Output "BIOS settings:"
 Get-CimInstance -ClassName Win32_BIOS
 
-#Write-Host "Welcome to the script of fetching computer Information"
-#Write-host "The BIOS Details are as follows"
-#Get-CimInstance -ClassName Win32_BIOS
-
 # List installed hotfixes
+$hotfixes = Get-CimInstance -ClassName Win32_QuickFixEngineering
 Write-Output ""
 Write-Output "Installed hotfixes:"
-Get-CimInstance -ClassName Win32_QuickFixEngineering
+Write-Output $hotfixes
 
 }
 
@@ -60,10 +59,13 @@ function network_info {
 
 Write-Output "" #newline
 Write-Output "Network information"
+Write-Output ""
 
 # get ipv4 addresses
-Write-Output "All IPv4 addresses:"
-Get-NetIPAddress -AddressFamily IPV4
+$ipaddresses = Get-NetIPAddress -AddressFamily IPV4
+Write-Output ""
+Write-Output "All IPv4 addresses:" "$ipaddresses"
+Write-Output ""
 
 }
 
@@ -76,13 +78,17 @@ function user_info {
 
 Write-Output "" #newline
 Write-Output "User information"
+Write-Output ""
 
 # all users
+Write-Output ""
 Write-Output "All users on the system:"
+Write-Output ""
 $users = Get-LocalUser
 foreach ($user in $users) {
     Write-Output "$user"
 }
+Write-Output ""
 
 }
 
@@ -95,11 +101,11 @@ function services_info {
 
 Write-Output "" #newline
 Write-Output "Information about processes and services"
+Write-Output ""
 
-#Write-Host "All running processes:"
-
-Write-Output "Status of the running services are as follows:"
-Get-CimInstance -ClassName Win32_Service | Format-Table -Property Status,Name,DisplayName -AutoSize -Wrap
+Write-Output ""
+Write-Output "All running services:"
+Get-Service | where{$_.Status -eq "Running"}
 
 }
 
@@ -115,10 +121,11 @@ Get-CimInstance -ClassName Win32_Service | Format-Table -Property Status,Name,Di
 # Run parts #
 #############
 
-system_info | Tee-Object -FilePath ".\system_info.txt"
-network_info | Tee-Object -FilePath ".\network_info.txt"
-user_info | Tee-Object -FilePath ".\user_info.txt"
-services_info | Tee-Object -FilePath ".\services_info.txt"
+system_info | Tee-Object -FilePath ".\$(hostname)_system_info.txt"
+network_info | Tee-Object -FilePath ".\$(hostname)_network_info.txt"
+user_info | Tee-Object -FilePath ".\$(hostname)_user_info.txt"
+services_info | Tee-Object -FilePath ".\$(hostname)_services_info.txt"
+
 
 Write-Output ""
 Write-Output "Internal Recon complete"
